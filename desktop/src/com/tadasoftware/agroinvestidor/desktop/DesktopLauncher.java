ec89acc3f0f184da2ff8df219bac7bba2163a1c2
Junior Tada <juniortada@gmail.com>
2017-04-15 16:50:16 -0300

    resolucao maior

--- desktop/src/com/tadasoftware/agroinvestidor/desktop/DesktopLauncher.java
+++ desktop/src/com/tadasoftware/agroinvestidor/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.tadasoftware.agroinvestidor.Agroinvestidor;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1360;
+		config.height = 760;
 		new LwjglApplication(new Agroinvestidor(), config);
 	}
 }
