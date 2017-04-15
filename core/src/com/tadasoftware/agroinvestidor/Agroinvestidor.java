932692511989fc38b72bf4184d417bdd0e0f032a
Leonardo Tada <leonardo_tada@hotmail.com>
2017-04-15 17:40:34 -0300

    tilesets e tilemap

--- core/src/com/tadasoftware/agroinvestidor/Agroinvestidor.java
+++ core/src/com/tadasoftware/agroinvestidor/Agroinvestidor.java
@@ -5,15 +5,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
 
 public class Agroinvestidor extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TiledMap map;
+  OrthographicCamera camera;
+  TiledMapRenderer tiledMapRenderer;
 
 	@Override
 	public void create () {
+		float w = Gdx.graphics.getWidth();
+    float h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		camera = new OrthographicCamera();
+    camera.setToOrtho(false,w,h);
+    camera.update();
+		map = new TmxMapLoader().load("mapa.tmx");
+    tiledMapRenderer = new OrthogonalTiledMapRenderer(map);
 	}
 
 	@Override
@@ -22,6 +39,9 @@ public class Agroinvestidor extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		camera.update();
+    tiledMapRenderer.setView(camera);
+    tiledMapRenderer.render();
 		batch.end();
 	}
 
