22e32fa8f3bfbc033a3edd42d53a1830b96972a8
Junior Tada <juniortada@gmail.com>
2017-04-15 19:43:42 -0300

    teste com botao

--- core/src/com/tadasoftware/agroinvestidor/Agroinvestidor.java
+++ core/src/com/tadasoftware/agroinvestidor/Agroinvestidor.java
@@ -10,27 +10,46 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+//import com.badlogic.gdx.scenes.scene2d.ui.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 
 
 public class Agroinvestidor extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	TiledMap map;
-  OrthographicCamera camera;
-  TiledMapRenderer tiledMapRenderer;
+  	OrthographicCamera camera;
+  	TiledMapRenderer tiledMapRenderer;
+  	TextButton.TextButtonStyle textButtonStyle;
+
+  	Stage stage;
+  	Skin skin;
 
 	@Override
 	public void create () {
 		float w = Gdx.graphics.getWidth();
-    float h = Gdx.graphics.getHeight();
+    	float h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 
 		camera = new OrthographicCamera();
-    camera.setToOrtho(false,w,h);
-    camera.update();
+    	camera.setToOrtho(false,w,h);
+    	camera.update();
 		map = new TmxMapLoader().load("mapa.tmx");
-    tiledMapRenderer = new OrthogonalTiledMapRenderer(map);
+    	tiledMapRenderer = new OrthogonalTiledMapRenderer(map);
+
+    	// botao
+    	stage = new Stage();
+    	createBasicSkin();
+    	TextButton botao = new TextButton("teste", skin);
+    	botao.setPosition(Gdx.graphics.getWidth()/2 - Gdx.graphics.getWidth()/8 , Gdx.graphics.getHeight()/2);
+    	stage.addActor(botao);
 	}
 
 	@Override
@@ -40,9 +59,13 @@ public class Agroinvestidor extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		camera.update();
-    tiledMapRenderer.setView(camera);
-    tiledMapRenderer.render();
+    	tiledMapRenderer.setView(camera);
+    	tiledMapRenderer.render();
 		batch.end();
+
+		Gdx.input.setInputProcessor(stage);
+		stage.act();
+        stage.draw();
 	}
 
 	@Override
@@ -50,4 +73,30 @@ public class Agroinvestidor extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+
+
+	public void createBasicSkin(){
+		//Create a font
+		BitmapFont font = new BitmapFont();
+		skin = new Skin();
+		skin.add("default", font);
+
+		//Create a texture
+		Pixmap pixmap = new Pixmap((int)Gdx.graphics.getWidth()/4,(int)Gdx.graphics.getHeight()/10, Pixmap.Format.RGB888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.fill();
+		skin.add("background",new Texture(pixmap));
+
+		//Create a button style
+		TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
+		textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
+		textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+		textButtonStyle.font = skin.getFont("default");
+		skin.add("default", textButtonStyle);
+
+	}
+
 }
