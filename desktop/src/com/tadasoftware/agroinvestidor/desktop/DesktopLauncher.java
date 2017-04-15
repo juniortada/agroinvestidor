9b396fcea229e66e55fe33bc34ab6947b5c8aa94
Leonardo Tada <leonardo_tada@hotmail.com>
2017-04-15 15:50:31 -0300

    inicial

---
+++ desktop/src/com/tadasoftware/agroinvestidor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tadasoftware.agroinvestidor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tadasoftware.agroinvestidor.Agroinvestidor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Agroinvestidor(), config);
+	}
+}
