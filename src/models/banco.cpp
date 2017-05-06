6c2ba8b158eecd455bfbb3ab89e2be5f0d34078c
Junior Tada <juniortada@gmail.com>
2017-05-06 14:35:43 -0300

    assets inicial

--- src/models/banco.cpp
+++ src/models/banco.cpp
@@ -1,1 +1,23 @@
-// classe banco
+#include "banco.hpp"
+
+void Banco::inicializa(float valor){
+	saldo = valor;
+}
+
+void Banco::deposita(float valor){
+	saldo = saldo + valor;
+}
+
+void Banco::consulta(){
+	cout << "Saldo atual: " << saldo << endl; 
+}
+
+void Banco::saque(float valor){
+	if(saldo < valor){
+		return 0;
+	}
+	else{
+		saldo = saldo - valor;
+		return 1;
+	}
+}
