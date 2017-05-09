a6f63698b23d22d829ace3f5ee0ba730cee65d1f
Leonardo <leonardo_tada@hotmail.com>
2017-05-09 20:39:28 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-# Build para sfml
-
-echo "ahh muleke"
-
-mkdir dist
-cd dist
-LOCAL_PATH=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
-echo $LOCAL_PATH
-
-echo ${LOCAL_PATH//dist/src}
-
-
-
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/detail/pugixml.cpp -o obj/Debug/modules/tmxlite/src/detail/pugixml.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/FreeFuncs.cpp -o obj/Debug/modules/tmxlite/src/FreeFuncs.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/ImageLayer.cpp -o obj/Debug/modules/tmxlite/src/ImageLayer.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/Map.cpp -o obj/Debug/modules/tmxlite/src/Map.o
+gcc -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/miniz.c -o obj/Debug/modules/tmxlite/src/miniz.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/Object.cpp -o obj/Debug/modules/tmxlite/src/Object.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/ObjectGroup.cpp -o obj/Debug/modules/tmxlite/src/ObjectGroup.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/Property.cpp -o obj/Debug/modules/tmxlite/src/Property.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/TileLayer.cpp -o obj/Debug/modules/tmxlite/src/TileLayer.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/modules/tmxlite/src/Tileset.cpp -o obj/Debug/modules/tmxlite/src/Tileset.o
+g++ -Wall -fexceptions -g -Imodules/tmxlite/include -c ~/Projetos/agroinvestidor/src/main.cpp -o obj/Debug/src/main.o
+g++  -o bin/Debug/teste obj/Debug/modules/tmxlite/src/detail/pugixml.o obj/Debug/modules/tmxlite/src/FreeFuncs.o obj/Debug/modules/tmxlite/src/ImageLayer.o obj/Debug/modules/tmxlite/src/Map.o obj/Debug/modules/tmxlite/src/miniz.o obj/Debug/modules/tmxlite/src/Object.o obj/Debug/modules/tmxlite/src/ObjectGroup.o obj/Debug/modules/tmxlite/src/Property.o obj/Debug/modules/tmxlite/src/TileLayer.o obj/Debug/modules/tmxlite/src/Tileset.o obj/Debug/src/main.o -lsfml-graphics -lsfml-window -lsfml-system
 
